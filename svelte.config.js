a9554bd55d47ca3dae6da5e5452b691350314068
Sergio Falcon <sfalcon@aisintn.com>
2025-02-10 11:31:07 -0500

    svelte adapter-dnode

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,6 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	},
-
     extensions: [".svelte", ".svx"]
 };
 
